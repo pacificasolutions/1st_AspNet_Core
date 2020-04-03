b88fe1ca28707081edaf08110ca5a9c74e97e984
Jagwinder Sekhon <jagwindersekhon@gmail.com>
2020-04-03 16:46:41 -0700

    First Commit

---
+++ AspnetTest/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using AspnetTest.Models;
+
+namespace AspnetTest.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
